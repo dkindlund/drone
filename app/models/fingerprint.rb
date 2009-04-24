b1fa6c53dd52af04721c556e3327ad91ca58b9c0
Darien Kindlund <github@kindlund.com>
2009-04-24 00:58:52 +0000

    Added logic to dynamically calculate checksums on supplied fingerprints.

--- app/models/fingerprint.rb
+++ app/models/fingerprint.rb
@@ -1,13 +1,16 @@
+require 'md5'
+
 class Fingerprint < ActiveRecord::Base
 
   has_one :url
   has_many :os_processes, :order => 'pid ASC', :before_add => :decrement_src_os_process_counter_cache, :after_add => :increment_dst_os_process_counter_cache, :after_remove => :decrement_os_process_counter_cache
 
-  validates_presence_of :checksum, :os_process_count
+  validates_presence_of :os_process_count
   validates_associated :os_processes
-  validates_length_of :checksum, :maximum => 255
+  validates_length_of :checksum, :allow_nil => true, :maximum => 255
   validates_numericality_of :os_process_count, :greater_than_or_equal_to => 0
 
+  before_create :calculate_checksum
   after_create :update_os_process_counter_cache
 
   def to_label
@@ -15,6 +18,53 @@ class Fingerprint < ActiveRecord::Base
   end
 
   private
+
+  # Calculates the fingerprint checksum, if need be.
+  def calculate_checksum()
+    # Don't calculate, if a checksum was already provided.
+    if not self.checksum.nil?
+        return
+    end
+
+    # Create an array of process strings, sort, and md5
+    process_strings = []
+    self.os_processes.each do |p|
+
+      # Begin string with process name.
+      process_string = p.name.to_s
+
+      # Append sorted files.
+      file_strings = []
+      p.process_files.each do |f|
+        if (f.event == "Delete" ||
+            f.file_content.nil? ||
+            f.file_content.md5  == "UNKNOWN" ||
+            f.file_content.sha1 == "UNKNOWN")
+          file_string = f.name.to_s
+        else
+          file_string = f.file_content.md5.to_s + f.file_content.sha1.to_s
+        end
+        file_string += f.event.to_s
+        file_strings << file_string
+      end
+      process_string += file_strings.sort.join("")
+
+      # Append sorted registries.
+      registry_strings = []
+      p.process_registries.each do |rk|
+        registry_string = rk.name.to_s
+        registry_string += rk.event.to_s
+        registry_string += rk.value_name.to_s
+        # XXX: Does not currently include value name or value type.
+        registry_strings << registry_string
+      end
+      process_string += registry_strings.sort.join("")
+      process_strings << process_string
+    end
+
+    # Calculate the corresponding checksum.
+    self.checksum = Digest::MD5.hexdigest(process_strings.sort.join(""))
+  end
 
   # XXX: These methods are probably inefficient, but its not clear how the
   # counter cache can be manipulated in any other way.
--- test/unit/fingerprint_test.rb
+++ test/unit/fingerprint_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class FingerprintTest < ActiveSupport::TestCase
-  test "should not save fingerprint without checksum" do
+  test "should save fingerprint without checksum" do
     c = Fingerprint.new
-    assert !c.save, "Saved fingerprint without checksum"
+    assert c.save, "Can't save fingerprint without checksum"
+    assert_equal c.checksum, "d41d8cd98f00b204e9800998ecf8427e", "Checksum incorrect"
   end
 
   test "should save duplicate fingerprints" do
