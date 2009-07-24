0485e8785ccfaf746684907f990e98ee4ba79deb
Darien Kindlund <github@kindlund.com>
2009-07-24 18:03:45 +0000

    Updated environment to use a shared, isolated ruby-inline directory.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Be sure to restart your server when you modify this file
 
+# Use production mode, by default.
+# ENV['RAILS_ENV'] ||= 'production'
+
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
@@ -55,6 +58,9 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
+  # Specify that the RubyInline directory should be beneath the rails root.
+  ENV['INLINEDIR'] = RAILS_ROOT
 end
 
 # XXX: Update this, accordingly.
